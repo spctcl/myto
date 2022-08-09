61c308c8608fa0c6ed823ec92f2fd0902fda278d
Erik Young <erik_young@mac.com>
2022-08-09 14:55:52 +0900

    Added dotenv.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const private_key = process.env.PRIVATE_KEY;
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>myto</Text>
       <StatusBar style="auto" />
     </View>
   );
